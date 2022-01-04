85ade27f29651967023ddc17530f693a390c25fa
kemotz <tomek_z@o2.pl>
2022-01-04 13:48:33 +0100

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include <Arduino.h>
+
+#if defined(GD32F30x) || defined(GD32E50X)
+  #define LED PB2
+#elif defined(GD32F3x0) || defined(GD32F1x0)
+    #define LED PC13  
+#endif
+
+
+#define BUTTON_PIN PB11
+
+unsigned flag=0;
+
+void button_isr() {
+if(flag)
+  flag=0;
+else 
+  flag=1;
+}
+
+void setup()
+{
+  attachInterrupt(BUTTON_PIN, &button_isr, FALLING);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(LED, OUTPUT);
+
+
+}
+
+void loop()
+{
+
+ delay(50);
+ digitalWrite(LED,flag);
+
+}
